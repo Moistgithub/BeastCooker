332c7674463f92640f0bc7265eedffa307d272bf
Moistgithub <mustafairfan321@gmail.com>
2024-08-02 02:39:53 +0800

    Working Attacks and somewhat functionable dodge!!!!!!
    The attack is perfect but now you need to focus on making the pizza roll good

--- Assets/Scripts/ForPlayer/PlayerAttack.cs
+++ Assets/Scripts/ForPlayer/PlayerAttack.cs
@@ -6,41 +6,49 @@ public class PlayerAttack : MonoBehaviour
 {
     PlayerMovement playermovement;
     public GameObject attackPoint;
-    private float attackTime = 0.5f;
+    private float attackTime = 0.3f;
+    private Vector3 lastAttackPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         playermovement = GetComponent<PlayerMovement>();
     }
-    private void FixedUpdate()
-    {
-        attackPoint.transform.localPosition = playermovement.movementDir * 2;
-    }
     public void Attack()
     {
+        //creates scenario where the player isnt moving but turns the attack direction to the last used
+        if (playermovement.movementDir.x != 0 || playermovement.movementDir.y != 0)
+        {
+            lastAttackPosition = playermovement.movementDir;
+        }
+        //uses reference from player movement to change location of attack point
+
+        attackPoint.transform.localPosition = lastAttackPosition; //playermovement.movementDir;
+
         attackPoint.SetActive(true);
+        StartCoroutine(TimeHandler());
         Debug.Log("Im running");
     }
 
     IEnumerator TimeHandler()
     {
-        Attack();
+        //Handles the countdown of 0.3 seconds for the attacks lifetime
         yield return new WaitForSeconds(attackTime);
         Dissapear();
         Debug.Log("Time " + attackTime);
     }   
     public void Dissapear()
     {
+        //attack literally dies
         attackPoint.SetActive(false);
         Debug.Log("Im not running");
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("z"))
+        if(Input.GetMouseButtonDown(0))
         {
-            StartCoroutine(TimeHandler());
+            Attack();
         }
     }
 }
--- Assets/Scripts/ForPlayer/PlayerMovement.cs
+++ Assets/Scripts/ForPlayer/PlayerMovement.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //reguular variables
     public float speed;
     private Rigidbody2D rb;
-
-    //to see how much the players position has changed
     public Vector2 movementDir;
+    private Vector2 lastMovementDR;
+
+    //dodge variables
+    private Vector3 dodgerollDir;
+    private float dodgerollSpeed = 10f;
+    private PlayerState state;
+
+    //create a simple state handler
+    public enum PlayerState
+    {
+        Normal,
+        DodgeRolling,
+    }
+
+    private void Awake()
+    {
+        //deciding our first state
+        state = PlayerState.Normal;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -18,36 +36,52 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movementDir.x = Input.GetAxisRaw("Horizontal");
-        movementDir.y = Input.GetAxisRaw("Vertical");
-        movementDir = new Vector2(movementDir.x, movementDir.y).normalized;
-        Flip();
+        //make a switch case for normal and rolling for movement and the dodge roll movement
+        switch (state)
+        {
+            case PlayerState.Normal:
+                movementDir.x = Input.GetAxisRaw("Horizontal");
+                movementDir.y = Input.GetAxisRaw("Vertical");
+                movementDir = new Vector2(movementDir.x, movementDir.y).normalized;
+                
+                if (movementDir.x != 0 || movementDir.y != 0)
+                {
+                    lastMovementDR = movementDir;
+                }
+
+                if (Input.GetMouseButtonDown(1))
+                {
+                    dodgerollDir = lastMovementDR;
+                    dodgerollSpeed = 250f;
+                    state = PlayerState.DodgeRolling;
+                    Debug.Log("is rolling");
+                }
+                break;
+            case PlayerState.DodgeRolling:
+
+                float drsMultiplier = 5f;
+                dodgerollSpeed -= dodgerollSpeed * drsMultiplier * Time.deltaTime;
+                float drsminimum = 50f;
+
+                if(dodgerollSpeed < drsminimum)
+                {
+                    state = PlayerState.Normal;
+                }
+                break;
+        }
     }
-    void Flip()
-    {
-        if (movementDir.x > 0.1f)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-            Debug.Log("Is Right");
-        }
-        else if (movementDir.x < -0.1f)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-            Debug.Log("Is left");
-        }
-        else if (movementDir.y > 0.1f)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-            Debug.Log("Is up");
-        }
-        else if (movementDir.y < - 0.1f)
-        {
-            transform.localScale = new Vector3(1, -1, 1);
-            Debug.Log("Is down");
-        }
-    }
+
     void FixedUpdate()
     {
-        rb.velocity = movementDir * speed;
+        switch (state)
+        {
+            case PlayerState.DodgeRolling:
+                rb.velocity = dodgerollDir * dodgerollSpeed;
+            break;
+            case PlayerState.Normal:
+                rb.velocity = movementDir * speed;
+                break;
+        }
+
     }
 }
