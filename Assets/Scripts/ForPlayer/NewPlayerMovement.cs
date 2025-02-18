2e8fa085054e7bf0609fa32676149e1cb7d30d02
Moistgithub <mustafairfan321@gmail.com>
2025-02-18 12:46:37 +0800

    Smooth movement working

---
+++ Assets/Scripts/ForPlayer/NewPlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class NewPlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float playerSpeed;
+
+    private Vector2 movementInput;
+    private Vector2 smoothmovementInput;
+    private Vector2 smoothmovementVelocity;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        smoothmovementInput = Vector2.SmoothDamp(
+            smoothmovementInput, movementInput,
+            ref smoothmovementVelocity,
+            0.1f);
+        rigidBody.velocity = smoothmovementInput * playerSpeed;
+    }
+    private void OnMove(InputValue inputValue)
+    {
+        movementInput = inputValue.Get<Vector2>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
