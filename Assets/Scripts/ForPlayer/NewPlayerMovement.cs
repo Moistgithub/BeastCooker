bc893f2c7ac43333b47e2e823f28c2c20dbd786d
Moistgithub <mustafairfan321@gmail.com>
2025-02-25 13:45:18 +0800

    working dodgeroll with new movement

--- Assets/Scripts/ForPlayer/NewPlayerMovement.cs
+++ Assets/Scripts/ForPlayer/NewPlayerMovement.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class NewPlayerMovement : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
     public Rigidbody2D rigidBody;
     public float playerSpeed;
+    public float smoothbetweenTime;
+
     public AnimationCurve movementCurve;
+    public AnimationCurve dodgeCurve;
+     
     public Animator animator;
     //public float time;
-    public float smoothbetweenTime;
-    public SpriteRenderer spriteRenderer;
 
     private Vector2 movementInput;
     private Vector2 smoothmovementInput;
     private Vector2 smoothmovementVelocity;
 
+    // Dodge roll variables
+    public float dodgeRollCooldown;
+
+    public float dodgeRollSpeed;
+    public float dodgeRollDuration;
+
+    private Vector2 lastMoveDir;
+    private Vector2 dodgeRollDir;
+    private float dodgeRollEndTime;
+    private bool canDodgeRoll = true;
+    private bool isInvincible = false;
+
+    private enum PlayerState
+    {
+        Normal,
+        DodgeRolling
+    }
+
+    private PlayerState currentState;
+
     private void Awake()
     {
+        animator.SetBool("IsRolling", false);
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();    
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
@@ -29,20 +53,48 @@ public class NewPlayerMovement : MonoBehaviour
             ref smoothmovementVelocity,
             movementCurve.Evaluate(smoothbetweenTime));
         rigidBody.velocity = smoothmovementInput * playerSpeed;
+        switch (currentState)
+        {
+            case PlayerState.Normal:
+                if (movementInput.magnitude > 0.1f)
+                {
+                    animator.SetBool("IsWalking", true);
+                }
+                else
+                {
+                    animator.SetBool("IsWalking", false);
+                }
+                break;
 
-        if (movementInput.magnitude > 0.1f)
-        {
-            animator.SetBool("IsWalking", true);
+            case PlayerState.DodgeRolling:
+                animator.SetBool("IsWalking", false);
+                animator.SetBool("IsRolling", true);
+                if (Time.time >= dodgeRollEndTime)
+                {
+                    currentState = PlayerState.Normal;
+                    ResetDodgeRollCooldown();
+                    isInvincible = false;
+                    animator.SetBool("IsRolling", false);
+                }
+                break;
         }
-        else
+        if (currentState == PlayerState.DodgeRolling)
         {
-            animator.SetBool("IsWalking", false);
+            float dodgeRollSpeedFactor = dodgeCurve.Evaluate((Time.time - (dodgeRollEndTime - dodgeRollDuration)) / dodgeRollDuration);
+            rigidBody.velocity = dodgeRollDir * dodgeRollSpeed * dodgeRollSpeedFactor;
         }
-        animator.SetBool("IsRolling", false);
+        if (!canDodgeRoll && Time.time >= dodgeRollEndTime + dodgeRollCooldown)
+        {
+            canDodgeRoll = true;
+        }
     }
     private void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
+        if (movementInput.magnitude > 0.1f)
+        {
+            lastMoveDir = movementInput.normalized;
+        }
     }
     private void FlipSpriteBasedOnMouse()
     {
@@ -60,6 +112,26 @@ public class NewPlayerMovement : MonoBehaviour
             spriteRenderer.flipX = true;
         }
     }
+    private void StartDodgeRoll()
+    {
+        //rolls when not moving to last movement made
+        dodgeRollDir = (movementInput.magnitude > 0.1f) ? movementInput.normalized : lastMoveDir;
+        //default move left if no input
+        if (dodgeRollDir.magnitude < 0.1f) dodgeRollDir = Vector2.left;
+
+        //dodge = current direction movement
+        //dodgeRollDir = movementInput.normalized;
+
+        dodgeRollEndTime = Time.time + dodgeRollDuration;
+        currentState = PlayerState.DodgeRolling;
+        isInvincible = true;
+        animator.SetBool("IsRolling", true);  // Trigger the roll animation
+        canDodgeRoll = false;  // Disable further dodge rolls until cooldown
+    }
+
+    public void ResetDodgeRollCooldown()
+    {
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -72,6 +144,11 @@ public class NewPlayerMovement : MonoBehaviour
     void Update()
     {
         FlipSpriteBasedOnMouse();
-        //FlipSpriteBasedOnMouse();
+        if (Input.GetMouseButtonDown(1) && canDodgeRoll)
+        {
+            {
+                StartDodgeRoll();
+            }
+        }
     }
 }
